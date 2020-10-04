03b90e929b37feb54982b72ab9636680ee7dbd25
Vinicius Silva <viniciuspsw@gmail.com>
2020-10-04 00:14:49 -0300

    add pwa support

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,6 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addon-knobs/register'
+    '@storybook/addon-essentials'
   ]
 }
--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,4 +1,3 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Story, Meta } from '@storybook/react/types-6-0'
 
 import Main from '.'
@@ -6,12 +5,10 @@ import Main from '.'
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'My amazing title',
+    description: 'My amazing description'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text('Description', 'TypeScript, ReactJS, NextJS e Styled Components')}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
