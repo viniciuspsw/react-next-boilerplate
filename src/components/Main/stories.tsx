7bc6dd3e62bfd74c35b142a20c75a13c77b402bf
Vinicius Silva <viniciuspsw@gmail.com>
2020-10-03 23:58:49 -0300

    set up storybook

---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,17 @@
+import { withKnobs, text } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react/types-6-0'
+
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+} as Meta
+
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text('Description', 'TypeScript, ReactJS, NextJS e Styled Components')}
+  />
+)
